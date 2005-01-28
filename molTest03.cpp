c299f44dd83a25b7a997c219c0f8777a751da872
Pavol Juhas <pavol.juhas@gmail.com>
2005-01-28 01:19:50 +0000

    added test of operator>>

--- molTest03.cpp
+++ molTest03.cpp
@@ -6,6 +6,7 @@
 * $Id$
 ***********************************************************************/
 
+#include <unistd.h>
 #include "BGAlib.hpp"
 
 int main()
@@ -15,26 +16,53 @@ int main()
     // distance table
     SandSphere ssHex(100, "hexagon.dss");
 
-    Molecule mol(&ssHex);
-    mol.Clear();
-    mol.ReadXY("hexagon.dxy");
+    Molecule mol1(&ssHex), mol2(&ssHex);
+    mol1.Clear();
+    mol1.ReadXY("hexagon.dxy");
 
-    cout << "mol:  MBadness() = " << mol.MBadness() << endl;
-    for (int i = 0; i < mol.NAtoms; ++i)
+    cout << "mol1:  MBadness() = " << mol1.MBadness() << endl;
+    for (int i = 0; i < mol1.NAtoms; ++i)
     {
-	cout << "ABadness(" << i << ") = " << mol.ABadness(i) << "  ";
-	cout << "AFitness(" << i << ") = " << mol.AFitness(i) << '\n';
+	cout << "ABadness(" << i << ") = " << mol1.ABadness(i) << "  ";
+	cout << "AFitness(" << i << ") = " << mol1.AFitness(i) << '\n';
     }
     cout << endl;
 
-    cout << "mol.OutFmtGrid():" << endl;
-    cout << mol.OutFmtGrid() << endl;
+    cout << "mol1.OutFmtGrid():" << endl;
+    cout << mol1.OutFmtGrid() << endl;
 
-    cout << "mol.OutFmtXY():" << endl;
-    cout << mol.OutFmtXY() << endl;
+    cout << "mol1.OutFmtXY():" << endl;
+    cout << mol1.OutFmtXY() << endl;
 
-    cout << "mol.OutFmtAeye():" << endl;
-    cout << mol.OutFmtAeye() << endl;
+    cout << "mol1.OutFmtAeye():" << endl;
+    cout << mol1.OutFmtAtomEye() << endl;
 
+    cout << "reading mol2 from Grid format" << endl;
+    mol1.WriteGrid("molTest03.tmp");
+    ifstream fid("molTest03.tmp");
+    fid >> mol2;
+    fid.close();
+    cout << "mol2:  MBadness() = " << mol2.MBadness() << endl;
+    for (int i = 0; i < mol2.NAtoms; ++i)
+    {
+	cout << "ABadness(" << i << ") = " << mol2.ABadness(i) << "  ";
+	cout << "AFitness(" << i << ") = " << mol2.AFitness(i) << '\n';
+    }
+    cout << endl;
+
+    cout << "reading mol2 from XY format" << endl;
+    mol1.WriteXY("molTest03.tmp");
+    fid.open("molTest03.tmp");
+    fid >> mol2;
+    fid.close();
+    cout << "mol2:  MBadness() = " << mol2.MBadness() << endl;
+    for (int i = 0; i < mol2.NAtoms; ++i)
+    {
+	cout << "ABadness(" << i << ") = " << mol2.ABadness(i) << "  ";
+	cout << "AFitness(" << i << ") = " << mol2.AFitness(i) << '\n';
+    }
+    cout << endl;
+
+    unlink("molTest03.tmp");
     return EXIT_SUCCESS;
 }
