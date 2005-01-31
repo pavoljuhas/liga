8f445077650a50f956bfe83914cd6e790a030815
Pavol Juhas <pavol.juhas@gmail.com>
2005-01-31 17:16:06 +0000

    more tests added

--- molTest04.cpp
+++ molTest04.cpp
@@ -1,8 +1,8 @@
 /***********************************************************************
 * Short Title: test of BGAlib.cpp
 *
-* Comments: test Molecule operators Shift(), Center(), Part(), Pop(),
-*     Add(), MoveAtom()
+* Comments: test Molecule operators: operator=, copy constructor,
+*     Shift(), Center(), Part(), Pop(), Add(), MoveAtomTo()
 *
 * $Id$
 ***********************************************************************/
@@ -24,80 +24,50 @@ int main()
     mol1.PrintBadness();
     cout << endl;
 
-    Molecule mol2 = mol1;
-//    Molecule mol2(&ssHex);
+    Molecule mol2(&ssHex);
 
+    cout << "mol2: testing mol2 = mol1:" << endl;
     mol2 = mol1;
-    cout << "mol2:\n";
-    mol2.PrintBadness();
-    cout << endl;
+    cout << mol2; mol2.PrintBadness(); cout << endl;
 
-    cout << mol2 << endl;
+    cout << "testing mol2.Shift(502, 0):" << endl;
     mol2.Shift(502, 0);
-    cout << mol2 << endl;
-    cout << "mol2:\n";
-    mol2.PrintBadness();
+    cout << mol2; mol2.PrintBadness(); cout << endl;
+
+    cout << "testing mol2.Center():" << endl;
+    mol2.Center();
+    cout << mol2; mol2.PrintBadness(); cout << endl;
+
+    cout << "testing mol2.Pop(0):" << endl;
+    mol2.Pop(0);
+    cout << mol2; mol2.PrintBadness(); cout << endl;
+
+    cout << "testing mol2.Add(50, 0):" << endl;
+    mol2.Add(50, 0);
+    cout << mol2; mol2.PrintBadness(); cout << endl;
+
+    cout << "testing Molecule mol3(mol2); mol3.Part(idx); mol2.Pop(idx):\n";
+    int i1[3] = {0, 1, 2}; list<int> idx(i1, i1+3);
+    Molecule mol3(mol2);
+    mol3.Part(idx);
+    mol2.Pop(idx);
+    cout << "mol2:" << endl << mol2; mol2.PrintBadness();
+    cout << "mol3:" << endl << mol3; mol3.PrintBadness();
     cout << endl;
 
-    cout << "mol1:\n";
-    mol1.PrintBadness();
+    cout << "testing mol3.Add(mol2):" << endl;
+    mol3.Add(mol2);
+    cout << "mol3:" << endl << mol3; mol3.PrintBadness();
     cout << endl;
 
-    /*
-    Molecule& Shift(int dh, int dk);	// shift all atoms
-    Molecule& Center();			// center w/r to the center of mass
-    Molecule& Part(const list<int>& cidx);	// keep only specified atoms
-    Molecule& Pop(const list<int>& cidx);	// remove specified atoms
-    Molecule& Pop(const int cidx);	// remove 1 atom
-    Molecule& Clear();			// remove all atoms
-    Molecule& Add(Molecule& m);		// add specified molecule
-    Molecule& Add(int nh, int nk);	// add single atom
-    Molecule& MoveAtom(int idx, int nh, int nk);	// move 1 atom
-
-    cout << "mol1:  MBadness() = " << mol1.MBadness() << endl;
-    for (int i = 0; i < mol1.NAtoms; ++i)
-    {
-	cout << "ABadness(" << i << ") = " << mol1.ABadness(i) << "  ";
-	cout << "AFitness(" << i << ") = " << mol1.AFitness(i) << '\n';
-    }
+    cout << "testing mol3.MoveAtomTo(0, 0, 7):" << endl;
+    mol3.MoveAtomTo(0, 0, 7);
+    cout << "mol3:" << endl << mol3; mol3.PrintBadness();
+    cout << endl;
+    cout << "testing mol3.MoveAtomTo(0, 25, 43):" << endl;
+    mol3.MoveAtomTo(0, 25, 43);
+    cout << "mol3:" << endl << mol3; mol3.PrintBadness();
     cout << endl;
 
-    cout << "mol1.OutFmtGrid():" << endl;
-    cout << mol1.OutFmtGrid() << endl;
-
-    cout << "mol1.OutFmtXY():" << endl;
-    cout << mol1.OutFmtXY() << endl;
-
-    cout << "mol1.OutFmtAeye():" << endl;
-    cout << mol1.OutFmtAtomEye() << endl;
-
-    cout << "reading mol2 from Grid format" << endl;
-    mol1.WriteGrid("molTest03.tmp");
-    ifstream fid("molTest03.tmp");
-    fid >> mol2;
-    fid.close();
-    cout << "mol2:  MBadness() = " << mol2.MBadness() << endl;
-    for (int i = 0; i < mol2.NAtoms; ++i)
-    {
-	cout << "ABadness(" << i << ") = " << mol2.ABadness(i) << "  ";
-	cout << "AFitness(" << i << ") = " << mol2.AFitness(i) << '\n';
-    }
-    cout << endl;
-
-    cout << "reading mol2 from XY format" << endl;
-    mol1.WriteXY("molTest03.tmp");
-    fid.open("molTest03.tmp");
-    fid >> mol2;
-    fid.close();
-    cout << "mol2:  MBadness() = " << mol2.MBadness() << endl;
-    for (int i = 0; i < mol2.NAtoms; ++i)
-    {
-	cout << "ABadness(" << i << ") = " << mol2.ABadness(i) << "  ";
-	cout << "AFitness(" << i << ") = " << mol2.AFitness(i) << '\n';
-    }
-    cout << endl;
-
-    unlink("molTest03.tmp");
-    */
     return EXIT_SUCCESS;
 }
