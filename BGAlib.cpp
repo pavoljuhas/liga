b12c89b97cf51f9632cb62d35c378be5fa7f76fd
Pavol Juhas <pavol.juhas@gmail.com>
2005-02-01 16:51:58 +0000

    fixed pointer typoes

--- BGAlib.cpp
+++ BGAlib.cpp
@@ -914,14 +914,14 @@ ostream& operator<<(ostream& fid, Molecule& M)
 	    if (!M.cached) M.calc_df();
 	    double xyz_extremes[6] = {
 		-M.ss->dmax,
-		1.01*M.ss->delta*min_element(M.h.begin(), M.h.end()),
-		1.01*M.ss->delta*min_element(M.k.begin(), M.k.end()),
+		1.01*M.ss->delta*(*min_element(M.h.begin(), M.h.end())),
+		1.01*M.ss->delta*(*min_element(M.k.begin(), M.k.end())),
 		M.ss->dmax,
-		1.01*M.ss->delta*max_element(M.h.begin(), M.h.end()),
-		1.01*M.ss->delta*max_element(M.k.begin(), M.k.end())
+		1.01*M.ss->delta*(*max_element(M.h.begin(), M.h.end())),
+		1.01*M.ss->delta*(*max_element(M.k.begin(), M.k.end()))
 	    };
-	    double xyz_lo = min_element(xyz_extremes, xyz_extremes+6);
-	    double xyz_hi = max_element(xyz_extremes, xyz_extremes+6);
+	    double xyz_lo = *min_element(xyz_extremes, xyz_extremes+6);
+	    double xyz_hi = *max_element(xyz_extremes, xyz_extremes+6);
 	    double xyz_rng = xyz_hi - xyz_lo;
 	    fid << "# BGA molecule format = atomeye" << endl;
 	    fid << "# NAtoms = " << M.NAtoms << endl;
