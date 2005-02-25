47931c04f27ad093861643f1acd78f69a24a385e
Pavol Juhas <pavol.juhas@gmail.com>
2005-02-25 00:06:22 +0000

    ParseArgs::getpar() renamed to GetPar(), added ParseArgs::GetParVec()

--- ParseArgs.hpp
+++ ParseArgs.hpp
@@ -42,8 +42,9 @@ public:
     void Parse();
     void List();
     void ReadPars(const char *file);
-    template <class T> T getpar(string p);
-    template <class T> T getpar(string p, T v);
+    template <class T> T GetPar(string par);
+    template <class T> T GetPar(string par, T defval);
+    template <typename T> vector<T> GetParVec(string par);
 private:
     void init();
     void do_getopt();
@@ -51,22 +52,37 @@ private:
     void arg_or_par(const char *s);
 };
 
-template<class T> T ParseArgs::getpar(string p)
+template<class T> T ParseArgs::GetPar(string par)
 {
-    if (!pars.count(p))
+    if (!pars.count(par))
     {
-	cerr << "parameter '" << p << "' is not defined" << endl;
+	cerr << "parameter '" << par << "' is not defined" << endl;
 	throw InvalidParameter();
     }
-    T v;
-    istringstream iss(pars[p]);
-    iss >> v;
-    return v;
+    T val;
+    istringstream iss(pars[par]);
+    iss >> val;
+    return val;
 }
 
-template<class T> T ParseArgs::getpar(string p, T v)
+template<class T> T ParseArgs::GetPar(string par, T defval)
 {
-    return pars.count(p) ? getpar<T>(p) : v;
+    return pars.count(par) ? GetPar<T>(par) : defval;
+}
+
+template <typename T> vector<T> GetParVec(string par)
+{
+    if (!pars.count(par))
+    {
+	cerr << "parameter '" << par << "' is not defined" << endl;
+	throw InvalidParameter();
+    }
+    vector<T> v;
+    istringstream iss(pars[par]);
+    T val;
+    while (iss >> val)
+	v.push_back(val);
+    return v;
 }
 
 #endif
