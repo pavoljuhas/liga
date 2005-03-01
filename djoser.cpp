970d4fd6ec7a9c659918dbca7e61a90495c37b9d
Pavol Juhas <pavol.juhas@gmail.com>
2005-03-01 00:49:08 +0000

    fixed help string

--- djoser.cpp
+++ djoser.cpp
@@ -16,11 +16,11 @@ void print_version()
 {
     using namespace std;
     const char *ver =
-	"$Id$"
+        "$Id$"
 #   if defined(__DATE__) && defined(__TIME__)
-	"\ncompiled " __DATE__ " " __TIME__
+        "\ncompiled " __DATE__ " " __TIME__
 #   endif
-	"\n";
+        "\n";
     cout << ver;
 }
 
@@ -47,7 +47,7 @@ void print_help(ParseArgs& a)
 "Walk parameters\n"
 "  tol_dd=double         [0.1] distance is not used when dd=|d-d0|>=tol_dd\n"
 "  tol_bad=double        [1E-4] target normalized molecule badness\n"
-"  seed=int        	 seed random number generator\n"
+"  seed=int              seed of random number generator\n"
 "  logsize=int           [10] last steps used for success rate evaluation\n"
 "  eprob_max=double      [0.75] high limit of evolve probability\n"
 "  eprob_min=double      [0.25] low limit of evolve probability\n"
@@ -90,68 +90,68 @@ struct RunPar_t
 Molecule process_arguments(RunPar_t& rp, int argc, char *argv[])
 {
     char *short_options =
-	"p:hv";
+        "p:hv";
     // parameters and options
     option long_options[] = {
-	{"parfile", 1, 0, 'p'},
-	{"help", 0, 0, 'h'},
-	{"version", 0, 0, 'v'},
-	{0, 0, 0, 0}
+        {"parfile", 1, 0, 'p'},
+        {"help", 0, 0, 'h'},
+        {"version", 0, 0, 'v'},
+        {0, 0, 0, 0}
     };
     ParseArgs a(argc, argv, short_options, long_options);
     try {
-	a.Parse();
-//	a.Dump();
+        a.Parse();
+//      a.Dump();
     }
     catch (ParseArgsError) {
-	exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
     if (a.isopt("h") || argc == 1)
     {
-	print_help(a);
-	exit(EXIT_SUCCESS);
+        print_help(a);
+        exit(EXIT_SUCCESS);
     }
     else if (a.isopt("v"))
     {
-	print_version();
-	exit(EXIT_SUCCESS);
+        print_version();
+        exit(EXIT_SUCCESS);
     }
     if (a.isopt("p"))
     {
-	try {
-	    a.ReadPars(a.opts["p"].c_str());
-	}
-	catch (IOError(e)) {
-	    cerr << e.what() << endl;
-	    exit(EXIT_FAILURE);
-	}
-	catch (ParseArgsError(e)) {
-	    cerr << "invalid syntax in parameter file" << endl;
-	    cerr << e.what() << endl;
-	    exit(EXIT_FAILURE);
-	}
+        try {
+            a.ReadPars(a.opts["p"].c_str());
+        }
+        catch (IOError(e)) {
+            cerr << e.what() << endl;
+            exit(EXIT_FAILURE);
+        }
+        catch (ParseArgsError(e)) {
+            cerr << "invalid syntax in parameter file" << endl;
+            cerr << e.what() << endl;
+            exit(EXIT_FAILURE);
+        }
     }
     // assign run parameters
     // distfile
     if (a.args.size())
-	a.pars["distfile"] = a.args[0];
+        a.pars["distfile"] = a.args[0];
     if (!a.ispar("distfile"))
     {
-	cerr << "Distance file not defined" << endl;
-	exit(EXIT_FAILURE);
+        cerr << "Distance file not defined" << endl;
+        exit(EXIT_FAILURE);
     }
     rp.distfile = a.pars["distfile"];
     DistanceTable* dtab;
     try {
-	dtab = new DistanceTable(rp.distfile.c_str());
+        dtab = new DistanceTable(rp.distfile.c_str());
     }
     catch (IOError) {
-	exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
     string hashsep(72, '#');
     cout << hashsep << endl;
     cout << "# " << a.cmd_t << ' ' <<
-	"$Id$" << endl;
+        "$Id$" << endl;
     time_t cur_time = time(NULL);
     cout << "# " << ctime(&cur_time);
     cout << hashsep << endl;
@@ -159,27 +159,27 @@ Molecule process_arguments(RunPar_t& rp, int argc, char *argv[])
     cout << "distfile=" << rp.distfile << endl;
     if (a.ispar("outstru"))
     {
-	rp.outstru = a.pars["outstru"];
-	cout << "outstru=" << rp.outstru << endl;
+        rp.outstru = a.pars["outstru"];
+        cout << "outstru=" << rp.outstru << endl;
     }
     if (a.ispar("inistru"))
     {
-	rp.inistru = a.pars["inistru"];
-	cout << "inistru=" << rp.inistru << endl;
+        rp.inistru = a.pars["inistru"];
+        cout << "inistru=" << rp.inistru << endl;
     }
     if (a.ispar("snapshot"))
     {
-	rp.snapshot = a.pars["snapshot"];
-	cout << "snapshot=" << rp.snapshot << endl;
-	rp.snaprate = a.GetPar<int>("snaprate", 100);
-	cout << "snaprate=" << rp.snaprate << endl;
+        rp.snapshot = a.pars["snapshot"];
+        cout << "snapshot=" << rp.snapshot << endl;
+        rp.snaprate = a.GetPar<int>("snaprate", 100);
+        cout << "snaprate=" << rp.snaprate << endl;
     }
     if (a.ispar("frames"))
     {
-	rp.frames = a.pars["frames"];
-	cout << "frames=" << rp.frames << endl;
-	rp.framesrate = a.GetPar<int>("framesrate", 100);
-	cout << "framesrate=" << rp.framesrate << endl;
+        rp.frames = a.pars["frames"];
+        cout << "frames=" << rp.frames << endl;
+        rp.framesrate = a.GetPar<int>("framesrate", 100);
+        cout << "framesrate=" << rp.framesrate << endl;
     }
     // Walk parameters
     rp.tol_dd = a.GetPar<double>("tol_dd", 0.1);
@@ -190,8 +190,8 @@ Molecule process_arguments(RunPar_t& rp, int argc, char *argv[])
     rp.seed = a.GetPar<int>("seed", 0);
     if (rp.seed)
     {
-	gsl_rng_set(BGA::rng, rp.seed);
-	cout << "seed=" << rp.seed << endl;
+        gsl_rng_set(BGA::rng, rp.seed);
+        cout << "seed=" << rp.seed << endl;
     }
     rp.logsize = a.GetPar<int>("logsize", 10);
     cout << "logsize=" << rp.logsize << endl;
@@ -209,15 +209,15 @@ Molecule process_arguments(RunPar_t& rp, int argc, char *argv[])
     mol.evolve_frac = rp.evolve_frac;
     rp.penalty = a.GetPar<string>("penalty", "pow2");
     if (rp.penalty == "pow2")
-	mol.penalty = BGA::pow2;
+        mol.penalty = BGA::pow2;
     else if (rp.penalty == "well")
-	mol.penalty = BGA::well;
+        mol.penalty = BGA::well;
     else if (rp.penalty == "fabs")
-	mol.penalty = fabs;
+        mol.penalty = fabs;
     else
     {
-	cerr << "Invalid value of penalty parameter" << endl;
-	exit(EXIT_FAILURE);
+        cerr << "Invalid value of penalty parameter" << endl;
+        exit(EXIT_FAILURE);
     }
     cout << "penalty=" << rp.penalty << endl;
     rp.dist_trials = a.GetPar("dist_trials", 10);
@@ -235,15 +235,15 @@ double prob_evolve(const Molecule& mol, RunPar_t& rp, double impr_rate)
     double pe;
     if (mol.NAtoms() == mol.max_NAtoms())
     {
-	pe = 0.0;
-	rp.bustprob = false;
+        pe = 0.0;
+        rp.bustprob = false;
     }
     else if (mol.NAtoms() <= 1)
-	pe = 1.0;
+        pe = 1.0;
     else if (rp.bustprob)
-	pe = 1.0;
+        pe = 1.0;
     else
-	pe = impr_rate*(rp.eprob_max-rp.eprob_min)+rp.eprob_min;
+        pe = impr_rate*(rp.eprob_max-rp.eprob_min)+rp.eprob_min;
     return pe;
 }
 
@@ -251,21 +251,21 @@ void evolve_or_degenerate(Molecule& mol, RunPar_t& rp, double pe)
 {
     if (pe > gsl_rng_uniform(BGA::rng))
     {
-	mol.Evolve(rp.dist_trials, rp.tri_trials, rp.pyr_trials);
-	cout << " E " << mol.NAtoms() << " " << mol.NormBadness();
+        mol.Evolve(rp.dist_trials, rp.tri_trials, rp.pyr_trials);
+        cout << " E " << mol.NAtoms() << " " << mol.NormBadness();
     }
     else
     {
-	int Npop = 1;
-	if (mol.NormBadness() > rp.tol_bad)
-	{
-	    Npop = (int) ceil( mol.NAtoms()/4.0 *
-		    (1.0 - rp.tol_bad/mol.NormBadness()) );
-	    //		would this help?
-//	    Npop = 1 + gsl_rng_uniform_int(BGA::rng, Npop);
-	}
-	mol.Degenerate(Npop);
-	cout << " D " << mol.NAtoms() << " " << mol.NormBadness();
+        int Npop = 1;
+        if (mol.NormBadness() > rp.tol_bad)
+        {
+            Npop = (int) ceil( mol.NAtoms()/4.0 *
+                    (1.0 - rp.tol_bad/mol.NormBadness()) );
+            //          would this help?
+//          Npop = 1 + gsl_rng_uniform_int(BGA::rng, Npop);
+        }
+        mol.Degenerate(Npop);
+        cout << " D " << mol.NAtoms() << " " << mol.NormBadness();
     }
     cout << endl;
 }
@@ -277,25 +277,25 @@ void save_snapshot(Molecule& mol, RunPar_t& rp)
     static int largest = 0;
     static double bestMNB = numeric_limits<double>().max();
     if (rp.snapshot.size() == 0 || rp.snaprate == 0 || ++cnt < rp.snaprate)
-	return;
+        return;
     if (  mol.NAtoms() > largest ||
-	    (mol.NAtoms() == largest && mol.NormBadness() < bestMNB)  )
+            (mol.NAtoms() == largest && mol.NormBadness() < bestMNB)  )
     {
-	largest = mol.NAtoms();
-	bestMNB = mol.NormBadness();
-	mol.WriteAtomEye(rp.snapshot.c_str());
-	cnt = 0;
+        largest = mol.NAtoms();
+        bestMNB = mol.NormBadness();
+        mol.WriteAtomEye(rp.snapshot.c_str());
+        cnt = 0;
     }
 }
 
 void save_frames(Molecule& mol, RunPar_t& rp, int iteration,
-	bool lastframe = false)
+        bool lastframe = false)
 {
 //  numeric_limits<double> double_info;
     static int cnt = 0;
     if (  rp.frames.size() == 0 || rp.framesrate == 0 ||
-	    (++cnt < rp.framesrate && !lastframe) )
-	return;
+            (++cnt < rp.framesrate && !lastframe) )
+        return;
     ostringstream oss;
     oss << rp.frames << "." << iteration;
     mol.WriteAtomEye(oss.str().c_str());
@@ -315,41 +315,41 @@ int main(int argc, char *argv[])
     int iteration = 0;
     while (true)
     {
-	++iteration;
-	// calculate probability of evolution
-	double impr_rate = 1.0*improved.sum()/rp.logsize;
-	if (impr_rate >= 0.5 && rp.bustprob > gsl_rng_uniform(BGA::rng))
-	    bust_now = true;
-	double pe = prob_evolve(mol, rp, impr_rate);
-	cout << iteration;
-	evolve_or_degenerate(mol, rp, pe);
-//	if (rp.show_abad)
-//	    mol.PrintBadness();
-	// update bestMNBadness and improved
-	int ilog = iteration % rp.logsize;
-	if (mol.NormBadness() <= bestMNBadness[mol.NAtoms()])
-	{
-	    bestMNBadness[mol.NAtoms()] = mol.NormBadness();
-	    improved[ilog] = 1;
-	}
-	else
-	{
-	    improved[ilog] = 0;
-	    if (bestMNBadness[mol.NAtoms()] < rp.tol_bad)
-		bestMNBadness[mol.NAtoms()] = rp.tol_bad;
-	}
-	save_snapshot(mol, rp);
-	save_frames(mol, rp, iteration);
-	if (mol.NAtoms() == mol.max_NAtoms() && mol.NormBadness() < rp.tol_bad)
-	{
-	    cout << "Solution found!!!" << endl;
-	    break;
-	}
+        ++iteration;
+        // calculate probability of evolution
+        double impr_rate = 1.0*improved.sum()/rp.logsize;
+        if (impr_rate >= 0.5 && rp.bustprob > gsl_rng_uniform(BGA::rng))
+            bust_now = true;
+        double pe = prob_evolve(mol, rp, impr_rate);
+        cout << iteration;
+        evolve_or_degenerate(mol, rp, pe);
+//      if (rp.show_abad)
+//          mol.PrintBadness();
+        // update bestMNBadness and improved
+        int ilog = iteration % rp.logsize;
+        if (mol.NormBadness() <= bestMNBadness[mol.NAtoms()])
+        {
+            bestMNBadness[mol.NAtoms()] = mol.NormBadness();
+            improved[ilog] = 1;
+        }
+        else
+        {
+            improved[ilog] = 0;
+            if (bestMNBadness[mol.NAtoms()] < rp.tol_bad)
+                bestMNBadness[mol.NAtoms()] = rp.tol_bad;
+        }
+        save_snapshot(mol, rp);
+        save_frames(mol, rp, iteration);
+        if (mol.NAtoms() == mol.max_NAtoms() && mol.NormBadness() < rp.tol_bad)
+        {
+            cout << "Solution found!!!" << endl;
+            break;
+        }
     }
     // save last frame
     save_frames(mol, rp, iteration, true);
     // save final structure
     if (rp.outstru.size() != 0)
-	mol.WriteAtomEye(rp.outstru.c_str());
+        mol.WriteAtomEye(rp.outstru.c_str());
     return EXIT_SUCCESS;
 }
