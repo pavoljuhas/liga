1deb82c5dca522ea31ff94d586ffcaee941ef6c5
Pavol Juhas <pavol.juhas@gmail.com>
2005-04-24 04:47:59 +0000

    implemented posToDist and readPlainPos

--- dvar.py
+++ dvar.py
@@ -13,4 +13,34 @@ Options:
 
 __id__ = "$Id$"
 
+def posToDist(r):
+    """calculate sorted list of distances from a given coordinate matrix
+    return scipy.array"""
+    from scipy import array, Float
+    from scipy.linalg import norm
 
+    d = [ norm(r[i]-r[j]) for i in range(len(r)) for j in range(i+1,len(r)) ]
+    d.sort()
+    ad = array(d)
+    return array(d)
+
+def readPlainPos(file):
+    """read plain coordinates from open file
+    return scipy matrix"""
+    from scipy import array, Float
+    
+    rlist = []
+    file.seek(0)
+    for l in file:
+        if l[0] == '#':  continue
+        rlist.append( [ float(w) for w in l.split()[0:3] ] )
+    rarray = array(rlist, Float)
+    return rarray
+
+if __name__ == "__main__":
+    file = open("/u24/juhas/programs/BGA/solids/icosahedron.xyz")
+    r = readPlainPos(file)
+    file.close()
+    d = posToDist(r)
+    for v in d:
+        print v
