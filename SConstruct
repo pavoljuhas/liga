diffpy/diffpy.srreal

Add `python` scons variable for selecting interpreter.
Using sys.executable for running setup.py would install to the same
Python as runs scons.  Now we can install for python in virtualenv
or for python2.6.

SConstruct
@@ -59,6 +59,8 @@ vars.Add(EnumVariable('build',
     allowed_values=('debug', 'fast')))
 vars.Add(BoolVariable('profile',
     'build with profiling information', False))
+vars.Add('python',
+    'Python executable to use for installation.', 'python')
 vars.Update(env)
 env.Help(MY_SCONS_HELP % vars.GenerateHelpText(env))
 

pavoljuhas/liga

SConstruct
@@ -50,6 +50,8 @@ vars.Add(EnumVariable('tool',
     allowed_values=('default', 'intelc')))
 vars.Add(BoolVariable('profile',
     'build with profiling information', False))
+vars.Add('python',
+    'Python executable to use for installation.', 'python3')
 vars.Add(PathVariable('prefix',
     'installation prefix directory', '/usr/local'))
 vars.Update(env)